20b1cc09513f65ee897919e6970c6eb30e05a3a3
Misclone <50257525+Misclone@users.noreply.github.com>
2019-05-30 18:25:31 +0900

    Add files via upload

--- Pi-v2.rb
+++ Pi-v2.rb
@@ -1,11 +1,11 @@
-puts "1~7桁の数字を入力してください"
+puts "1~10桁の数字を入力してください"
 
 str = File.read("C:/Users/intern8/Ruby/Pi/3355pi.txt")
 
 
 x = gets.chomp!
 
-if x !~ /^\d{1,7}$/
+if x !~ /^\d{1,10}$/
 
   puts "入力が正しくありません"
 
